4a0bcc1123b8c71f1b44c7562558538056b7b718
asmyasnikov <79263256394@ya.ru>
2022-07-13 00:38:00 +0300

    fix flapping of TestIntersectTraceIDs

--- storage/spanstore/dbmodel/unique_ids_test.go
+++ storage/spanstore/dbmodel/unique_ids_test.go
@@ -1,11 +1,20 @@
 package dbmodel
 
 import (
+	"sort"
+	"strings"
 	"testing"
 
 	"github.com/jaegertracing/jaeger/model"
 	"github.com/stretchr/testify/assert"
 )
+
+func sortTraceIDs(traceIDs []TraceID) []TraceID {
+	sort.Slice(traceIDs, func(i, j int) bool {
+		return strings.Compare(string(traceIDs[i][:]), string(traceIDs[j][:])) < 0
+	})
+	return traceIDs
+}
 
 func TestIntersectTraceIDs(t *testing.T) {
 	a := NewUniqueTraceIDs()
@@ -17,7 +26,7 @@ func TestIntersectTraceIDs(t *testing.T) {
 	b.Add(TraceIDFromDomain(model.NewTraceID(1, 3)))
 	b.Add(TraceIDFromDomain(model.NewTraceID(1, 4)))
 
-	result := IntersectTraceIDs([]*UniqueTraceIDs{a, b}).AsList()
-	expected := []TraceID{TraceIDFromDomain(model.NewTraceID(1, 2)), TraceIDFromDomain(model.NewTraceID(1, 3))}
+	result := sortTraceIDs(IntersectTraceIDs([]*UniqueTraceIDs{a, b}).AsList())
+	expected := sortTraceIDs([]TraceID{TraceIDFromDomain(model.NewTraceID(1, 2)), TraceIDFromDomain(model.NewTraceID(1, 3))})
 	assert.Equal(t, expected, result)
 }
